65b804ab2b28a84067ce88cc507326f1c14f0834
Will Ayd <william.ayd@icloud.com>
2017-08-25 06:31:15 -0400

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 setup(name='BOATParser',
-      version='0.1.1',
+      version='0.1.2',
       description='Business Objects Admin Tools Parser',
-      long_descfription=README,
+      long_description=README,
       author='Will Ayd',
       author_email='william.ayd@icloud.com',
       py_modules=['BOATParser'],
